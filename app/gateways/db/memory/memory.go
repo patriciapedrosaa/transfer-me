5413ca09515f395fd33561383e2ca9ec01f34069
Patrícia Pedrosa <patricia.pedrosaa@gmail.com>
2021-07-20 15:16:33 -0300

    refactor: refact transfer memory storage

--- app/gateways/db/memory/memory.go
+++ app/gateways/db/memory/memory.go
@@ -2,10 +2,10 @@ package memory
 
 type MemoryStorage struct {
 	storageAccount  map[string]Account
-	storageTransfer map[string]Transfer
+	storageTransfer map[string][]Transfer
 }
 
-func NewMemoryStorage(storageAccount map[string]Account, storageTransfer map[string]Transfer) MemoryStorage {
+func NewMemoryStorage(storageAccount map[string]Account, storageTransfer map[string][]Transfer) MemoryStorage {
 	return MemoryStorage{
 		storageAccount:  storageAccount,
 		storageTransfer: storageTransfer,
