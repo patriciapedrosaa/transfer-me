606a28ff3462bb04a9cc619c9433a6ace3b08c13
Patrícia Pedrosa <patricia.pedrosaa@gmail.com>
2021-09-10 17:05:27 -0300

    refactor: remove typo

--- app/domain/account/usecase/get_balance.go
+++ app/domain/account/usecase/get_balance.go
@@ -10,9 +10,7 @@ func (a Account) GetBalance(ctx context.Context, id string) (int, error) {
 		Msg("getting balance for account id")
 	account, err := a.GetById(ctx, id)
 	if err != nil {
-		a.logger.Error().Err(err).
-			Str("account_ID", id).
-			Msg("error occurred when was trying get balance for id")
+		log.Error().Err(err).Msg("error occurred when was trying to get balance for id")
 		return 0, err
 	}
 	return account.Balance, nil
